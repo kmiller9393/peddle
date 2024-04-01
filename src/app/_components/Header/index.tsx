a46174440517f6c8c6c651ae191ce908c0836846
Kurt Miller <kmiller9393@gmail.com>
2024-03-31 20:31:15 -0600

    add LayoutHeader, add product images

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -3,9 +3,11 @@
 }
 
 import React from 'react'
+import Link from 'next/link'
 
 import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
+import LayoutHeader from './LayoutHeader'
 
 export async function Header() {
   let header: Header | null = null
@@ -16,8 +18,12 @@ export async function Header() {
     // When deploying this template on Payload Cloud, this page needs to build before the APIs are live
     // So swallow the error here and simply render the header without nav items if one occurs
     // in production you may want to redirect to a 404  page or at least log the error somewhere
-    // console.error(error)
+    console.error(error)
   }
 
-  return <header></header>
+  return (
+    <>
+      <LayoutHeader header={header} />
+    </>
+  )
 }
